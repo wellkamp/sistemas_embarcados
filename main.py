0b3b37c237714368e5747b74f5072badc27bb55d
wellkamp <wellkamp@gmail.com>
2020-09-21 13:54:15 -0300

    v1

---
+++ main.py
@@ -0,0 +1,29 @@
+import ufirebase as firebase
+import machine
+import utime
+import gc
+import micropython
+
+URL = 'esp32well/esp32'
+sair = 0
+
+
+def troca_led(URL):
+    if firebase.get(URL) == 1:
+        print('acende led 1')
+        print('apaga led 2')
+    else:
+        print('acende led 2')
+        print('apaga led 1')
+
+
+while sair != True:
+    temp = 0
+    for temp in range(0, 2):
+        utime.sleep(15)
+        gc.collect()
+        troca_led(URL)
+        utime.sleep(15)
+        firebase.put(URL, temp)
+        gc.collect()
+        utime.sleep(15)
---
+++ startup.py
@@ -0,0 +1,15 @@
+import sys
+
+def wlan_connect(ssid='WELLSUH 2G', password='01530134'):
+    import network
+    wlan = network.WLAN(network.STA_IF)
+    if not wlan.active() or not wlan.isconnected():
+        wlan.active(True)
+        print('connecting to:', ssid)
+        wlan.connect(ssid, password)
+        while not wlan.isconnected():
+            pass
+    print('network config:', wlan.ifconfig())
+    print('Conectado')
+
+wlan_connect()
